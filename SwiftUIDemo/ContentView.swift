e39475b7819ebaaf7bd750c2788461d47e74bb98
SwiftUIDemo <tkdan3024@naver.com>
2021-12-29 18:43:31 -0800

    @State private var text: String = "Welcome to SwiftUI"
    TextField("Enter text here", text: $text)
    .textFieldStyle(RoundedBorderTextFiledStyle())

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var rotation: Double = 0
+    @State private var text: String = "Welcome to SwiftUI"
     
     var body: some View {
         VStack {
@@ -17,7 +18,12 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .rotationEffect(.degrees(self.rotation))
+                .animation(.easeInOut(duration: 5))
+            
             Slider(value: $rotation, in: 0 ... 360, step: 0.1)
+            
+            TextField("Enter text here", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
         }
     }
 }
